d4dd3a33be23d07be1e45d88d6ee06a69459a463
Niv Zelber <nivznivz0@gmail.com>
2023-11-11 23:14:06 +0200

    Add Ex22 up until Targil 6

---
+++ Ex22.js
@@ -0,0 +1,34 @@
+// Targil 1
+alert("Sagiv");
+
+// Targil 2
+var x = 5;
+var y = 6;
+alert(x + y);
+
+// Targil 3
+var firstName = "Sagiv";
+var lastName = "Zilber";
+alert(firstName + " " + lastName);
+
+// Targil 4
+document.write("<a href='http://www.google.com'> GOOGLE </a>");
+
+// Targil 5
+document.write("<img src='images/1.jpg' />");
+
+// Targil 6
+document.write(`
+  <table>
+      <tbody>
+        <tr>
+          <td>
+            <a href='http://www.google.com'> GOOGLE </a>
+          </td>
+          <td>
+            <img src="images/1.jpg" />
+          </td>
+        </tr>
+      </tbody>
+  </table>
+`);
